5b24fc41b33e8897d53aba07a815bb119830856a
Pete Green <42935860+plgreenLIRU@users.noreply.github.com>
2025-07-15 17:21:19 +0100

    Multi dimensional inputs (#5)
    * starting 2D example
    
    * looks like it's working
    
    * adding test for 2d case
    
    * version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PoE_GPFlow",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=["gpflow"],
     entry_points={
--- tests/test_generalised_PoE.py
+++ tests/test_generalised_PoE.py
@@ -27,3 +27,40 @@ def test_1D():
     X_new = np.linspace(0, 10, 100).reshape(-1, 1)
     mean, var, beta = m.predict(X_new)
     assert(np.allclose(mean, np.sin(X_new), atol=0.05))
+
+def test_2D():
+
+    # Define the true function
+    def f(x, y):
+        return np.sin(x) * np.cos(y)
+
+    # Generate training data
+    np.random.seed(2)
+    n_train = 200
+    x_train = np.random.uniform(0, 5, size=(n_train,))
+    y_train = np.random.uniform(0, 5, size=(n_train,))
+    X_train = np.vstack((x_train, y_train)).T
+    sigma = 0.1
+    z_train = f(x_train, y_train) + sigma * np.random.randn(n_train)
+
+    # Generate test grid
+    x_lin = np.linspace(0, 5, 50)
+    y_lin = np.linspace(0, 5, 50)
+    X1, X2 = np.meshgrid(x_lin, y_lin)
+    X_test = np.vstack([X1.ravel(), X2.ravel()]).T
+
+    Z_true = f(X_test[:, 0], X_test[:, 1])
+
+    m = Generalised_PoE()
+    m.train(X=X_train, Y=np.vstack(z_train), no_experts=3, allow_seperate_hyperparms=False)
+
+    for i in range(3):
+        assert(np.allclose(m.models[i].kernel.lengthscales, m.joint_hyperparameters[0:2]))
+        assert(np.allclose(m.models[i].likelihood.variance, m.joint_hyperparameters[-1]))
+
+    # Check inferred noise variance
+    assert(np.allclose(m.joint_hyperparameters[-1], sigma**2, atol=0.01))
+
+    # Check mean predictions
+    mean, var, beta = m.predict(X_test)
+    assert(np.allclose(mean[:, 0], Z_true, atol=0.2))
